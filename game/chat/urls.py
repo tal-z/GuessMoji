06f0570713d588e7d73d17be28e1ff2aeea7506c
talzaken <talzaken@gmail.com>
2022-11-08 16:36:53 -0500

    Add room to urlpath

--- game/chat/urls.py
+++ game/chat/urls.py
@@ -6,7 +6,7 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("lobby/", views.lobby, name="lobby"),
-    path("<str:room_name>/", views.room, name="room"),
+    path("room/<str:room_name>/", views.room, name="room"),
     path("new_prompt/<str:room_name>/", views.new_prompt, name="new_prompt"),
     path("unauthorized", views.unauthorized, name="unauthorized")
 ]
