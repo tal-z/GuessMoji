85390ce62fa5d57b1561c85815928462adf1a95b
talzaken <talzaken@gmail.com>
2022-11-08 16:44:13 -0500

    Add sites framework

--- game/game/settings.py
+++ game/game/settings.py
@@ -19,6 +19,7 @@ load_dotenv()
 
 ENVIRONMENT = os.getenv("ENVIRONMENT")
 
+# Use pysqlite in production
 if ENVIRONMENT == "production":
     __import__('pysqlite3')
     import sys
@@ -41,6 +42,7 @@ INSTALLED_APPS = [
     'daphne',
     'prompts',
     'chat',
+    'django.contrib.sites',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -78,11 +80,11 @@ TEMPLATES = [
     },
 ]
 
+# App locations
 WSGI_APPLICATION = 'game.wsgi.application'
-
 ASGI_APPLICATION = 'game.asgi.application'
 
-#
+# Environment-specific allowed hosts
 if ENVIRONMENT == "local":
     DEBUG = True
     ALLOWED_HOSTS = ["127.0.0.1"]
@@ -92,7 +94,7 @@ if ENVIRONMENT == "production":
     DEBUG = False
     ALLOWED_HOSTS = ["172.31.3.66", "guessmoji.us-east-1.elasticbeanstalk.com"]
 
-# Message Broker Info
+# Message Broker -- Currently Shared in All Environments
 RABBITMQ_USERNAME = os.getenv("RABBITMQ_USERNAME")
 RABBITMQ_PASSWORD = os.getenv("RABBITMQ_PASSWORD")
 RABBITMQ_BROKER_URL = os.getenv("RABBITMQ_BROKER_URL")
@@ -107,7 +109,6 @@ CHANNEL_LAYERS = {
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -157,3 +158,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, "static")
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Sites Framework
+SITE_ID = 1
